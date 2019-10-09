c832f7641da6c7db8976deeb5fd02e00b35b25ef
SamuelMDubois <s.dubois@rubika-edu.com>
2019-10-09 15:21:02 +0200

    gumba est animé et les pipes sont bon

--- TileMapManu/Assets/EnnemyBehavior.cs
+++ TileMapManu/Assets/EnnemyBehavior.cs
@@ -7,16 +7,23 @@ public class EnnemyBehavior : MonoBehaviour
     public float speed;
     public Rigidbody2D ennemie;
     public BoxCollider2D box;
+     Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         ennemie.velocity = new Vector2(-speed, ennemie.velocity.y);
+    }
+
+    private void Update()
+    {
+        if (transform.position.y < -6)
+            Destroy(gameObject);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -27,24 +34,26 @@ public class EnnemyBehavior : MonoBehaviour
             if (collision.contacts[0].point.x > transform.position.x - 0.1f && collision.contacts[0].point.x < transform.position.x + 0.1f)
             {
                 StartCoroutine(Death());
-
             }
+
     }
 
     IEnumerator Death()
     {
-
+        // transition d'animation a faire
+        animator.SetBool("death", true);
+        speed = 0;
         yield return new WaitForSeconds(1);
+        Destroy(gameObject);
     }
 
     void KillBoop()
     {
         transform.localScale = -transform.localScale;
-        speed = -1;
+        speed = -5;
+        ennemie.velocity = new Vector2(speed, 16);
         box.isTrigger = true;
-
-        if (transform.position.y < -6)
-            Destroy(gameObject);
+       
     }
 
 }
