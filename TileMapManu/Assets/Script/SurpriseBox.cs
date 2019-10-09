bdc913a648c90977016f95dde0937dbb4843b3bc
AntoineG-88 <grugeon.antoine@gmail.com>
2019-10-09 12:03:42 +0200

    début box boop

---
+++ TileMapManu/Assets/Script/SurpriseBox.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurpriseBox : MonoBehaviour
+{
+    public GameObject storedItem;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.CompareTag("Player"))
+        {
+
+        }
+    }
+
+    private IEnumerator Boop()
+    {
+        yield return new WaitForSeconds(3);
+    }
+}
