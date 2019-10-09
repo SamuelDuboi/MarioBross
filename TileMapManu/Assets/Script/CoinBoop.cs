c02a2e801afacc2412c8bebe720f6b1a515f540f
AntoineG-88 <grugeon.antoine@gmail.com>
2019-10-09 14:36:39 +0200

    Surprise adeed

---
+++ TileMapManu/Assets/Script/CoinBoop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBoop : MonoBehaviour
+{
+    public float speed;
+    public float amplitude;
+    public float timeBeforeKill;
+
+    private float startYPos;
+    private float times;
+    private void Start()
+    {
+        times = 0;
+        startYPos = transform.position.y;
+        Invoke("Kill", timeBeforeKill);
+    }
+    void Update()
+    {
+        times += Time.deltaTime;
+        transform.position = new Vector2(transform.position.x, Mathf.Sin(times * speed) * amplitude);
+    }
+
+    void Kill()
+    {
+        Destroy(gameObject);
+    }
+}
--- TileMapManu/Assets/Script/SurpriseBox.cs
+++ TileMapManu/Assets/Script/SurpriseBox.cs
@@ -4,27 +4,89 @@ using UnityEngine;
 
 public class SurpriseBox : MonoBehaviour
 {
+    public Sprite solidBlockSprite;
     public GameObject storedItem;
+    [Range(0,20)]
+    public int storedCoins;
+    public GameObject coinAnimationO;
+    public float riseSpeed;
+
+    Collider2D hitCollider;
+    bool canBeBooped;
+    bool itemInside;
     void Start()
     {
-        
+        itemInside = true;
+        canBeBooped = true;
     }
 
     void Update()
     {
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D collider)
-    {
-        if(collider.CompareTag("Player"))
+        if(canBeBooped)
         {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartCoroutine(Boop());
+            }
 
+            hitCollider = Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y - 0.5f), new Vector2(1.0f, 0.1f), 0.0f, LayerMask.GetMask("Player"));
+            if (hitCollider != null)
+            {
+                StartCoroutine(Boop());
+            }
+
+            if (storedCoins <= 0)
+            {
+                canBeBooped = false;
+                GetComponent<SpriteRenderer>().sprite = solidBlockSprite;
+            }
         }
     }
 
     private IEnumerator Boop()
     {
-        yield return new WaitForSeconds(3);
+        float startYPos = transform.position.y;
+        float relativeTime = 0;
+        Collider2D aboveCollider = Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y + 0.5f), new Vector2(1.0f, 0.1f), 0.0f);
+        if(aboveCollider != null && aboveCollider.CompareTag("Ennemi"))
+        {
+            //aboveCollider.gameObject.GetComponent<EnnemiBehaviour>().KillBoop();
+        }
+
+        if (storedCoins > 0)
+        {
+            storedCoins--;
+            Instantiate(coinAnimationO, transform.position, Quaternion.identity);
+        }
+
+        while(transform.position.y >= startYPos)
+        {
+            transform.position = new Vector2(transform.position.x, startYPos + Mathf.Sin(relativeTime * 20) * 0.5f);
+            relativeTime += 0.02f;
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        transform.position = new Vector2(transform.position.x, startYPos);
+
+        if (storedItem != null && itemInside)
+        {
+            itemInside = false;
+            StartCoroutine(RiseItem(Instantiate(storedItem, new Vector2(transform.position.x, transform.position.y), Quaternion.identity)));
+
+        }
+    }
+
+    private IEnumerator RiseItem(GameObject item)
+    {
+        float relativeYPos = 0;
+        float startYPos = item.transform.position.y;
+        while(relativeYPos < 1.0f)
+        {
+            item.transform.position = new Vector2(transform.position.x, startYPos + relativeYPos);
+            relativeYPos += 0.02f * riseSpeed;
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        item.transform.position = new Vector2(transform.position.x, transform.position.y + 1.0f);
     }
 }
